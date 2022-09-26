dab86e0bb5757e72800941e7558a35fdea609311
datnguyenzzz <datnguyen79198@gmail.com>
2022-09-26 17:38:10 +0700

    init card quote table

--- src/Components/TableQuote.js
+++ src/Components/TableQuote.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../css/styles.css";
+import "../css/table-data.css";
 
 class TableQuote extends React.Component {
     constructor() {
@@ -10,8 +12,26 @@ class TableQuote extends React.Component {
 
     render() {
         return (
-            <div>
-                Quote data
+            <div className="card card-container table-data">
+                <div className="card-body">
+                    <h2 className="h6 mb-3">
+                        Latest available data:
+                    </h2>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">Stock Code</th>
+                            <th scope="col">Last Updated Data</th>
+                            <th scope="col">Open Price</th>
+                            <th scope="col">Low Price</th>
+                            <th scope="col">High Price</th>
+                            <th scope="col">Previous Close Price</th>
+                            <th scope="col">Current Price</th>
+                            <th scope="col">% From Yesterday</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         )
     };
